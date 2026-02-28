3f964cd2ad5b3ea332af4d55e25c7d3169d06028
bettercallmefifi <ferdaouskai@gmail.com>
2026-02-28 02:30:57 +0000

    fix some errors

--- ex4/ft_raise_errors.py
+++ ex4/ft_raise_errors.py
@@ -20,32 +20,32 @@ def check_plant_health(plant_name: str,
 
 def test_plant_checks() -> None:
     print("=== Garden Plant Health Checker ===")
-    print("Testing good values...")
+    print("\nTesting good values...")
     try:
         result = check_plant_health("tomato", 5, 6)
         print(result)
     except ValueError as error:
         print(f"Error: {error}")
 
-    print("Testing empty plant name...")
+    print("\nTesting empty plant name...")
     try:
         check_plant_health("", 5, 6)
     except ValueError as error:
         print(f"Error: {error}")
 
-    print("Testing bad water level...")
+    print("\nTesting bad water level...")
     try:
         check_plant_health("banana", 15, 4)
     except ValueError as error:
         print(f"Error: {error}")
 
-    print("Testing bad sunlight hours...")
+    print("\nTesting bad sunlight hours...")
     try:
         check_plant_health("appel", 3, 0)
     except ValueError as error:
         print(f"Error: {error}")
 
-    print("All error raising tests completed!")
+    print("\nAll error raising tests completed!")
 
 
 if __name__ == "__main__":
